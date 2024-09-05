5c4961b1284ea3dca6c63cb166d8cbbcc4040844
Rahul Mangal <mglrahul@gmail.com>
2024-09-05 12:40:58 +0530

    build the header, sign-in & signup form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="App">
-      Hello World, lets build first apps
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
